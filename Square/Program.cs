ece127ac3fd49b25bf611041dd160d27234d7390
Lev# <mr.lmartirosyan888@gmail.com>
2019-02-11 23:02:27 +0400

    relise one

---
+++ Square/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HomeWork6Loop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("If you want to draw square");
+            Console.WriteLine("Write what size square you want:");
+            int x = Convert.ToInt32(Console.ReadLine());
+            int y = x;
+
+            for (int i = 0; i < x; i++)
+            {
+                for (int r = 0; r < x; r++)
+                {
+                    Console.Write(" ");
+
+                }
+
+                for (int j = 0; j < x; j++)
+                {
+                    Console.Write("*");
+                }
+                Console.Write(" ");
+                Console.WriteLine("");
+            }
+            Console.ReadKey();
+        }
+    }
+}
